e141e320b40981ab2be4faa286b4b11e25440096
eblavender <ben.lavender@hotmail.com>
2020-06-18 17:06:38 +0100

    FINAL PUSH

--- GameJam 2020/Assets/Scripts/GameManager.cs
+++ GameJam 2020/Assets/Scripts/GameManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -142,7 +142,7 @@ public class GameManager : MonoBehaviour
     {
         foreach (germMultiply germ in allGerms)
         {
-            if (germ.hasMultiplied)
+            if (germ.hasMultiplied && allGerms.Count > 25)
                 continue;
 
             tempMultiply = germ.MultiplyGerm().GetComponent<germMultiply>();
